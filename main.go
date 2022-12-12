b415136950beba9926bd2750ab1ea5e474a3ad4f
Helmut Kemper <helmut.kemper@gmail.com>
2022-12-11 23:10:09 -0300

    static path

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./static"))
+	fs := http.FileServer(http.Dir("/go/static"))
 	http.Handle("/", fs)
 
 	log.Print("Stating server on port 3000")
